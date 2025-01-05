de77569bf815812dab58aa967986422380d9958e
Dejan Maksimovic <maxtoryusa@gmail.com>
2025-01-05 04:39:37 -0600

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Dejan Maksimovic on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
